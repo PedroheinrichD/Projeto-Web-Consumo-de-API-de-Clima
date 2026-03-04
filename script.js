426bfb5bcd12975a2945fd5f75e2ba38a062285d
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-03-04 09:34:47 -0300

    feat: integração com API de clima e proteção da API key via config.js e .gitignore

---
+++ script.js
@@ -0,0 +1,53 @@
+// API https://api.openweathermap.org/data/2.5/weather?q=Sao%20Paulo&appid=SUA_CHAVE_AQUI&units=metric&lang=pt
+// 404 error
+// 200 correct
+
+document.querySelector('.busca').addEventListener('click', async(event) => {
+    event.preventDefault()
+
+    const searchInput = document.querySelector('#searchInput').value
+    
+    if(searchInput !== ''){
+        showWarning('Carregando...')
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(searchInput)}&appid=${API_KEY}&units=metric&lang=pt`
+        let results = await fetch(url)
+        let json = await results.json()
+        console.log(json);
+        
+        
+    if(json.cod === 200){
+        showInfo({
+            name: json.name,
+            country: json.sys.country,
+            temp: json.main.temp,
+            tempIcon: json.weather[0].icon,
+            windspeed: json.wind.speed,
+            windAngle: json.wind.deg,
+        })
+    }else{
+        showWarning('Não encontramos esta localização')
+    }
+    }
+
+
+})
+
+function showInfo(json){
+    showWarning('')
+    document.querySelector('.resultado').style.display = 'block'
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`
+
+    
+    document.querySelector('.ventoInfo').innerHTML = `${json.windspeed} <span>km/h</span>`
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-180}deg)`
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}.png`)
+
+
+
+}
+
+
+function showWarning(msg){
+    document.querySelector('.aviso').innerHTML = msg
+}
